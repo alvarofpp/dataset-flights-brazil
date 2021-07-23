28ba9bc69b068ad5c4cb617e76e672e4a59c8f09
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-07-22 22:12:02 -0300

    refactor: extract script

--- extract.py
+++ extract.py
@@ -3,7 +3,9 @@ import requests
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 
-# Request
+path = 'data/extract/'
+
+# Flights (ANAC)
 url = 'https://www.gov.br/anac/pt-br/assuntos/dados-e-estatisticas/dados-estatisticos'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -17,9 +19,19 @@ links = [tag_a['href'] for tag_a in tags_a if tag_a['href'].endswith('.csv')]
 
 # Download all files
 for link in tqdm(links):
-    filename = 'data/' + link.split('/')[-1]
+    filename = path + link.split('/')[-1]
     if os.path.exists(filename):
         continue
 
     file_response = requests.get(link)
     open(filename, 'wb').write(file_response.content)
+
+# Public aerodromes (ANAC)
+url = 'https://sistemas.anac.gov.br/dadosabertos/Aerodromos/Lista%20de%20aer%C3%B3dromos%20p%C3%BAblicos/AerodromosPublicos.csv'
+file_response = requests.get(url)
+open(path + 'public_aerodromes.csv', 'wb').write(file_response.content)
+
+# Airport Codes (DataHub.io)
+url = 'https://datahub.io/core/airport-codes/r/airport-codes.csv'
+file_response = requests.get(url)
+open(path + 'airport_codes.csv', 'wb').write(file_response.content)
