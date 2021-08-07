03cbbdc30b7719b039aa74ebf035eaf3e1ee43b2
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-08-07 11:04:00 -0300

    fix: files path

--- transform_to_anac_csv.py
+++ transform_to_anac_csv.py
@@ -1,7 +1,7 @@
 import glob
 import pandas as pd
 
-files = glob.glob('data/resumo_anual_*.csv')
+files = glob.glob('data/extract/resumo_anual_*.csv')
 
 # Combine all files in the list
 combined_csv = pd.concat([pd.read_csv(file, sep=';', encoding='ISO-8859-1') for file in files])
